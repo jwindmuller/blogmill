6a6da4bf5c17d4451e46a176b02718b008b22fcb
Joaquin Windmuller <joaquin@Piet.local>
2011-03-22 14:25:54 -0600

    Fixing from field for contact form

--- controllers/contacts_controller.php
+++ controllers/contacts_controller.php
@@ -7,9 +7,11 @@ class ContactsController extends AppController {
     public function send() {
         if ($this->Contact->save($this->data)) {
             $this->Email->to = $this->Setting->get('BlogmillDefault.blogmill_contact_email'); 
-            $this->email->subject = $this->data['Contact']['subject'];
+            $this->Email->subject = $this->data['Contact']['subject'];
             $this->Email->replyTo = $this->data['Contact']['email'];
-            $this->Email->from =  $this->data['Contact']['email'];
+            $name  = $this->data['Contact']['name'];
+            $email = $this->data['Contact']['email'];
+            $this->Email->from = sprintf('%s <%s>', $name, $email);
             if ($this->Email->send($this->data['Contact']['message'])) {
                 $this->Session->setFlash(__('Thanks for contacting us, we\'ll be replying to you by mail soon', true), 'success');
                 $this->redirect(array('controller' => 'contacts', 'action' => 'send'));
