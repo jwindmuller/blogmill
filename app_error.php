2b18b4935b892c9e36f6cdcadc6e9f897dd07bb9
Joaquin Windmuller <joaquin@Piet.local>
2011-05-01 21:30:28 -0600

    Implementing installation of tables from plugins schemas.

--- app_error.php
+++ app_error.php
@@ -16,11 +16,29 @@ class AppError extends ErrorHandler {
 
     private function __createTablesFromSchema() {
         App::import('Model', 'CakeSchema');
+        // Main App Schema
         $schema = new CakeSchema(array('name' => 'App'));
         $schema = $schema->load();
         $db =& ConnectionManager::getDataSource($schema->connection);
         foreach ($schema->tables as $table => $fields) {
             $db->execute($db->createSchema($schema, $table));
+        }
+        // Load plugin schemas
+		$plugins = Configure::listObjects('plugin');
+        foreach( $plugins as $plugin ) {
+            $file = APP . 'plugins' . DS . Inflector::underscore($plugin) . DS . 'config' . DS . 'schema' . DS . 'schema.php';
+            $path = App::pluginPath($plugin) . 'config' . DS . 'schema';
+            $schema = new CakeSchema(array(
+                'name' => Inflector::underscore($plugin),
+                'plugin' => $plugin,
+                'path' => $path
+            ));
+            $schema = $schema->load();
+            if ($schema) {
+                foreach( $schema->tables as $table => $fields ) {
+                    $db->execute($db->createSchema($schema, $table));
+                }
+            }
         }
     }
  
