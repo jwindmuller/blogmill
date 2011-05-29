6fb19ca7f01d2b1d2bffa2fc14691183bf6895df
Joaquin Windmuller <joaquin@Piet.local>
2011-05-29 11:28:53 -0600

    Enabling and translating more strings

--- plugins/blogmill_default/config/settings.php
+++ plugins/blogmill_default/config/settings.php
@@ -1,39 +1,45 @@
 <?php
 class BlogmillDefaultSettings extends BlogmillSettings {
     var $types = array();
-    var $configurable = array(
+    var $configurable;
+    var $theme;
+
+    function __construct() {
+        $this->theme = array(
+    		'name' => __('Blogmill Default Theme', true),
+	    	'description' => __('Bare bones theme', true),
+		    'author' => 'Windmill Information Technology Inc.',
+    		'author_url' => 'http://windmuller.ca',
+	    	'version' => '1.0',
+		    'menus' => array(),
+    		'layouts' => array(
+	    		'home' => array(
+		    		'name' => 'default',
+			    	'load_menus' => array(),
+				    'data' => array(
+					    'posts' => array(
+						    'type' => '*',
+    						'limit' => 10,
+	    					'order' => 'Post.created DESC'
+		    			)
+			    	)
+    			),
+	    		'inner' => array(
+		    		'name' => 'default',
+			    	'load_menus' => array() 
+    			)
+	    	),
+		    'post_type_decorators' => array()
+    	);
+
+        $this->configurable = array(
         'blogmill_site_name' => array(
-            'label' => 'Website name'
+            'label' => __('Website name', true)
         ),
 		'blogmill_contact_email' => array(
-			'label' => 'Contact email',
-			'longdesc' => 'Write the contact email where you want to receive emails.'
+			'label' => __('Contact email', true),
+			'longdesc' => __('Write the contact email where you want to receive emails.', true)
 		),
 	);
-    var $theme = array(
-		'name' => 'Blogmill Default Theme',
-		'description' => 'Bare bones theme',
-		'author' => 'Windmill Information Technology Inc.',
-		'author_url' => 'http://windmuller.ca',
-		'version' => '1.0',
-		'menus' => array(),
-		'layouts' => array(
-			'home' => array(
-				'name' => 'default',
-				'load_menus' => array(),
-				'data' => array(
-					'posts' => array(
-						'type' => '*',
-						'limit' => 10,
-						'order' => 'Post.created DESC'
-					)
-				)
-			),
-			'inner' => array(
-				'name' => 'default',
-				'load_menus' => array() 
-			)
-		),
-		'post_type_decorators' => array()
-	);
+    }
 }
--- views/users/dashboard_index.ctp
+++ views/users/dashboard_index.ctp
@@ -29,7 +29,7 @@
 		<td class="actions">
 			<?php echo $this->Html->link(__('View', true), array('action' => 'view', $user['User']['id'])); ?>
 			<?php echo $this->Html->link(__('Edit', true), array('action' => 'edit', $user['User']['id'])); ?>
-			<?php echo $this->Html->link(__('Delete', true), array('action' => 'delete', $user['User']['id']), null, sprintf(__('Are you sure you want to delete # %s?', true), $user['User']['name'])); ?>
+			<?php echo $this->Html->link(__('Delete', true), array('action' => 'delete', $user['User']['id']), null, sprintf(__('Are you sure you want to delete the user %s?', true), $user['User']['name'])); ?>
             <?php if (!empty($user['User']['confirmation'])) : ?> |  
             <?php echo $this->Html->link(__('Notify', true), array('action' => 'notify', $user['User']['confirmation'])); ?>
             <?php endif; ?>
