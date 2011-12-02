6cffa3896c536ccb59877bf2db7fc50371a3e293
Joaquin Windmuller <joaquin@windmuller.ca>
2011-12-02 15:15:06 -0700

    Implementing a centralized way of launching hooks.
    BlogmillHook will try to load each plugin's hook component
    ({$plugin}HooksComponent} and execute the method $hook_name.
    
    Each hook method that receives parameters should return them
    in the same order.

--- controllers/components/blogmill_hook.php
+++ controllers/components/blogmill_hook.php
@@ -6,4 +6,28 @@ class BlogmillHookComponent extends Object {
 	public function startup($Controller) {
 		$this->Controller = $Controller;
 	}
+
+    public function call($hook_name, $params = array()) {
+        $hookName = Inflector::camelize($hook_name);
+        $plugins = Configure::listObjects('plugin');
+		foreach ($plugins as $i => $plugin) {
+            $plugin_folder = Inflector::underscore($plugin);
+            $hooks_path =
+                APP . 'plugins' . DS . $plugin_folder . DS .
+                'controllers' . DS . 'components' . DS;
+            $hookClass = "{$plugin}HooksComponent";
+            App::import(
+				array(
+					'type' => 'File',
+					'name' => $hookClass,
+					'file' => $hooks_path . $plugin_folder . '_hooks.php'
+				)
+			);
+            if (class_exists($hookClass)) {
+                $obj = new $hookClass;
+                call_user_func(array($obj, $hook_name), &$params);
+            }
+        }
+        return $params;
+    }
 }
