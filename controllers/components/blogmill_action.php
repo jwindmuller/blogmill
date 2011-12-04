ad816c4578be78f5a13679a6af9d7fd247a5cc26
Joaquin Windmuller <joaquin@windmuller.ca>
2011-12-03 22:43:25 -0700

    Fixing error whenreferencing Controller

--- controllers/components/blogmill_action.php
+++ controllers/components/blogmill_action.php
@@ -1,6 +1,12 @@
 <?php
 class BlogmillActionComponent extends Object {
     
+    protected $Controller;
+
+    public function startup($Controller) {
+        $this->Controller = $Controller;
+    }
+
     final function execute_plugin_action($plugin, $action) {
         $pluginName = Inflector::camelize($plugin);
         $path = App::pluginPath($plugin) . 'controllers' . DS . 'components' . DS . $plugin .'_actions.php';
