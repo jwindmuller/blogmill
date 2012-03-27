f7a2a10d100a3093b1be4ee2ae6fa971fbeb720d
Joaquin Windmuller <joaquin@windmuller.ca>
2012-03-26 21:31:36 -0600

    Changing tabs to spaces on default_plugin settings

--- plugins/blogmill_default/config/settings.php
+++ plugins/blogmill_default/config/settings.php
@@ -6,50 +6,50 @@ class BlogmillDefaultSettings extends BlogmillSettings {
 
     function __construct() {
         $this->theme = array(
-    		'name' => __('Blogmill Default Theme', true),
-	    	'description' => __('Bare bones theme', true),
-		    'author' => 'Windmill Information Technology Inc.',
-    		'author_url' => 'http://windmuller.ca',
-	    	'version' => '1.0',
-		    'menus' => array(),
-    		'layouts' => array(
-	    		'home' => array(
-		    		'name' => 'default',
-			    	'load_menus' => array(),
-				    'data' => array(
-					    'posts' => array(
-						    'type' => '*',
-    						'limit' => 10,
-	    					'order' => 'Post.created DESC'
-		    			)
-			    	)
-    			),
-	    		'inner' => array(
-		    		'name' => 'default',
-			    	'load_menus' => array() 
-    			)
-	    	),
-		    'post_type_decorators' => array()
-    	);
+            'name' => __('Blogmill Default Theme', true),
+            'description' => __('Bare bones theme', true),
+            'author' => 'Windmill Information Technology Inc.',
+            'author_url' => 'http://windmuller.ca',
+            'version' => '1.0',
+            'menus' => array(),
+            'layouts' => array(
+                'home' => array(
+                    'name' => 'default',
+                    'load_menus' => array(),
+                    'data' => array(
+                        'posts' => array(
+                            'type' => '*',
+                            'limit' => 10,
+                            'order' => 'Post.created DESC'
+                        )
+                    )
+                ),
+                'inner' => array(
+                    'name' => 'default',
+                    'load_menus' => array()
+                )
+            ),
+            'post_type_decorators' => array()
+        );
 
         $this->configurable = array(
-        'blogmill_site_name' => array(
-            'label' => __('Website name', true)
-        ),
-        'blogmill_site_description' => array(
-            'label' => __('Description or tagline for the site', true)
-        ),
-		'blogmill_contact_email' => array(
-			'label' => __('Contact email', true),
-			'longdesc' => __('Write the contact email where you want to receive emails.', true)
-		),
-		'blogmill_editor' => array(
-			'label' => __('Editor to use', true),
-			'longdesc' => __('Select the preferred editor to write html content.', true),
-            'by_user' => true,
-            'hookable' => true,
-            'default' => 'html'
-		),
-	);
+            'blogmill_site_name' => array(
+                'label' => __('Website name', true)
+            ),
+            'blogmill_site_description' => array(
+                'label' => __('Description or tagline for the site', true)
+            ),
+            'blogmill_contact_email' => array(
+                'label' => __('Contact email', true),
+                'longdesc' => __('Write the contact email where you want to receive emails.', true)
+            ),
+            'blogmill_editor' => array(
+                'label' => __('Editor to use', true),
+                'longdesc' => __('Select the preferred editor to write html content.', true),
+                'by_user' => true,
+                'hookable' => true,
+                'default' => 'html'
+            ),
+        );
     }
 }
